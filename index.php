179b6d8487bb1b057e1fa65dd276e0195d082f79
akuabonney <bonniebunny1235@gmail.com>
2016-11-17 23:32:37 +0000

    added index file, an image folder and a font folder

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<title>muzikiBot</title>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" type="text/css" href="css/animate.css">
+	<script src="js/library/jquery-2.1.1.min.js"></script>
+	<script src="js/library/jquery.cycle.all.js"></script>
+</head>
+<body>
+	<div id="welbx" onclick="addcss()">
+		<button id="buttx">Hi..! Check in.</button>
+	</div>
+</body>
+</html>
